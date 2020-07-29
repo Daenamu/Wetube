b0397f7d95a365075f01eed6844958896c82efc0
Daenamu <dheodnr318@naver.com>
2020-07-30 00:34:00 +0900

    install parser

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -18,6 +20,9 @@ const handleHome = (req, res) => {
   res.send('Hello from home');
 }
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser(urlencoded({ extended: true })));
 app.use(morgan("dev"));
 app.use(helmet());
 
