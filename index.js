71eeeddc788a002aa9dd7ec5153aa5da3c32c3dd
Daenamu <dheodnr318@naver.com>
2020-07-29 22:24:56 +0900

    Handling Routes

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+const handleProfile = (req, res) => {
+  res.send("You are on my profile");
+}
+
+const handleHome = (req, res) => {
+  res.send('Hello from home');
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(4000, handleListening);
