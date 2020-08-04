b6868cb693b87358534f26796079e6a8ddbe7644
Daenamu <dheodnr318@naver.com>
2020-08-04 19:12:40 +0900

    webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+// const autoprefixer = require("autoprefixer");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const ExtractCSS = require("extract-text-webpack-plugin");
+
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+};
+
+module.exports = config;
