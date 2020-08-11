68742ba822fd9fb3a03fac38881bc27da6945300
Daenamu <dheodnr318@naver.com>
2020-08-11 16:58:50 +0900

    multer upload to aws s3

--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,34 @@
+import dotenv from "dotenv";
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 import routes from "./routes";
+
+dotenv.config();
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_SECRET,
+  region: "ap-northeast-2",
+});
+
+const multerVideo = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wetube-daenamu/videos",
+  }),
+});
+const multerAvatar = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wetube-daenamu/avatars",
+  }),
+});
+
+export const uploadVideoMiddleware = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube";
@@ -23,9 +52,3 @@ export const onlyPrivate = (req, res, next) => {
     res.redirect(routes.home);
   }
 };
-
-const multerVideo = multer({ dest: "uploads/videos/" });
-const multerAvatar = multer({ dest: "uploads/avatars/" });
-
-export const uploadVideoMiddleware = multerVideo.single("videoFile");
-export const uploadAvatar = multerAvatar.single("avatar");
